569e9d17998074ffcbdc2091baa5f734764ae28c
dana <80922081+dananac@users.noreply.github.com>
2023-05-01 21:17:09 -0700

    Update test_*.py

--- test_*.py
+++ test_*.py
@@ -3,82 +3,83 @@ import numpy as np
 import data_storage as ds
 import decision_trees as dt
 
-# tests building numpy array
-def test_build_nparray():
-    file_name = "data1.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    assert ds.build_nparray(testData)
+class TestMethods(unittest.TestCase):
+    # tests building numpy array
+    def test_build_nparray(self):
+        file_name = "data1.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        self.assertTrue(ds.build_nparray(testData))
 
-# tests building list
-def test_build_list():
-    file_name = "data1.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    assert ds.build_list(testData)
+    # tests building list
+    def test_build_list(self):
+        file_name = "data1.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        self.assertTrue(ds.build_list(testData))
 
-# tests building dict
-def test_build_dict():
-    file_name = "data1.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    assert ds.build_dict(testData)
+    # tests building dict
+    def test_build_dict(self):
+        file_name = "data1.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        self.assertTrue(ds.build_dict(testData))
 
-# tests training data
-def test_train_data():
-    file_name = "data1.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    max_depth = 3
-    X,Y = ds.build_nparray(testData)
-    assert dt.DT_train_binary(X,Y,max_depth)
+    # tests training data
+    def test_train_data(self):
+        file_name = "data1.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        max_depth = 3
+        X,Y = ds.build_nparray(testData)
+        self.assertTrue(dt.DT_train_binary(X,Y,max_depth))
 
-# tests tree result
-def test_tree():
-    file_name = "data1.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    max_depth = 3
-    X,Y = ds.build_nparray(testData)
-    DT = dt.DT_train_binary(X,Y,max_depth)
-    assert dt.DT_test_binary(X,Y,DT)
+    # tests tree result
+    def test_tree(self):
+        file_name = "data1.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        max_depth = 3
+        X,Y = ds.build_nparray(testData)
+        DT = dt.DT_train_binary(X,Y,max_depth)
+        self.assertTrue(dt.DT_test_binary(X,Y,DT))
 
-# tests making prediction
-def test_make_prediction():
-    file_name = "data1.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    max_depth = 3
-    X,Y = ds.build_nparray(testData)
-    DT = dt.DT_train_binary(X,Y,max_depth)
-    assert dt.DT_make_prediction(Y,DT)
+    # tests making prediction
+    def test_make_prediction(self):
+        file_name = "data1.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        max_depth = 3
+        X,Y = ds.build_nparray(testData)
+        DT = dt.DT_train_binary(X,Y,max_depth)
+        self.assertTrue(dt.DT_make_prediction(Y,DT))
 
-# tests building tree
-def test_build_tree():
-    file_name = "data1.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    max_depth = 3
-    X,Y = ds.build_nparray(testData)
-    DT = dt.DT_train_binary(X,Y,max_depth)
-    assert dt.build_tree(X,Y,max_depth,DT)
+    # tests building tree
+    def test_build_tree(self):
+        file_name = "data1.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        max_depth = 3
+        X,Y = ds.build_nparray(testData)
+        DT = dt.DT_train_binary(X,Y,max_depth)
+        self.assertTrue(dt.build_tree(X,Y,max_depth,DT))
 
-# tests building tree with data for different edge cases
-def test_build_tree_data2():
-    file_name = "data2.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    max_depth = 3
-    X,Y = ds.build_nparray(testData)
-    DT = dt.DT_train_binary(X,Y,max_depth)
-    assert dt.build_tree(X,Y,max_depth,DT)
+    # tests building tree with data for different edge cases
+    def test_build_tree_data2(self):
+        file_name = "data2.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        max_depth = 3
+        X,Y = ds.build_nparray(testData)
+        DT = dt.DT_train_binary(X,Y,max_depth)
+        self.assertTrue(dt.build_tree(X,Y,max_depth,DT))
 
-# tests building tree with data for different edge cases
-def test_build_tree_data3():
-    file_name = "data3.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    max_depth = 3
-    X,Y = ds.build_nparray(testData)
-    DT = dt.DT_train_binary(X,Y,max_depth)
-    assert dt.build_tree(X,Y,max_depth,DT)
+    # tests building tree with data for different edge cases
+    def test_build_tree_data3(self):
+        file_name = "data3.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        max_depth = 3
+        X,Y = ds.build_nparray(testData)
+        DT = dt.DT_train_binary(X,Y,max_depth)
+        self.assertTrue(dt.build_tree(X,Y,max_depth,DT))
 
-# tests building tree with data for different edge cases
-def test_build_tree_data4():
-    file_name = "data4.csv"
-    testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
-    max_depth = 3
-    X,Y = ds.build_nparray(testData)
-    DT = dt.DT_train_binary(X,Y,max_depth)
-    assert dt.build_tree(X,Y,max_depth,DT)
+    # tests building tree with data for different edge cases
+    def test_build_tree_data3(self):
+        file_name = "data4.csv"
+        testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
+        max_depth = 3
+        X,Y = ds.build_nparray(testData)
+        DT = dt.DT_train_binary(X,Y,max_depth)
+        self.assertTrue(dt.build_tree(X,Y,max_depth,DT))
