aec0e9d55dc1083303540b67623fa311c63fb7d1
dana <80922081+dananac@users.noreply.github.com>
2023-05-01 21:01:03 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,25 +4,25 @@ import data_storage as ds
 import decision_trees as dt
 
 # tests building numpy array
-def test_build_nparray(self):
+def test_build_nparray():
     file_name = "data1.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     self.assertTrue(ds.build_nparray(testData))
 
 # tests building list
-def test_build_list(self):
+def test_build_list():
     file_name = "data1.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     self.assertTrue(ds.build_list(testData))
 
 # tests building dict
-def test_build_dict(self):
+def test_build_dict():
     file_name = "data1.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     self.assertTrue(ds.build_dict(testData))
 
 # tests training data
-def test_train_data(self):
+def test_train_data():
     file_name = "data1.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     max_depth = 3
@@ -30,7 +30,7 @@ def test_train_data(self):
     self.assertTrue(dt.DT_train_binary(X,Y,max_depth))
 
 # tests tree result
-def test_tree(self):
+def test_tree():
     file_name = "data1.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     max_depth = 3
@@ -39,7 +39,7 @@ def test_tree(self):
     self.assertTrue(dt.DT_test_binary(X,Y,DT))
 
 # tests making prediction
-def test_make_prediction(self):
+def test_make_prediction():
     file_name = "data1.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     max_depth = 3
@@ -48,7 +48,7 @@ def test_make_prediction(self):
     self.assertTrue(dt.DT_make_prediction(Y,DT))
 
 # tests building tree
-def test_build_tree(self):
+def test_build_tree():
     file_name = "data1.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     max_depth = 3
@@ -57,7 +57,7 @@ def test_build_tree(self):
     self.assertTrue(dt.build_tree(X,Y,max_depth,DT))
 
 # tests building tree with data for different edge cases
-def test_build_tree_data2(self):
+def test_build_tree_data2():
     file_name = "data2.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     max_depth = 3
@@ -66,7 +66,7 @@ def test_build_tree_data2(self):
     self.assertTrue(dt.build_tree(X,Y,max_depth,DT))
 
 # tests building tree with data for different edge cases
-def test_build_tree_data3(self):
+def test_build_tree_data3():
     file_name = "data3.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     max_depth = 3
@@ -75,7 +75,7 @@ def test_build_tree_data3(self):
     self.assertTrue(dt.build_tree(X,Y,max_depth,DT))
 
 # tests building tree with data for different edge cases
-def test_build_tree_data3(self):
+def test_build_tree_data4():
     file_name = "data4.csv"
     testData = np.genfromtxt(file_name, dtype=str, delimiter=',')
     max_depth = 3
